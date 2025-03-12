54dc2385c4656de086fc518f77710873326b26ee
taka-kawah <kawaharatakashi0724@gmail.com>
2025-03-12 20:39:54 +0900

    RoomのCRUDを追加

--- back/domain/room.go
+++ back/domain/room.go
@@ -1,1 +1,8 @@
 package domain
+
+import "gorm.io/gorm"
+
+type Room struct {
+	gorm.Model
+	Name string `validate:"required" db:"name"`
+}
--- back/infra/room_repo.go
+++ back/infra/room_repo.go
@@ -1,1 +1,60 @@
 package infra
+
+import (
+	"back/domain"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type RoomDriver struct {
+	gormDb *gorm.DB
+}
+
+func NewRoomDriver(gormDb *gorm.DB) *RoomDriver {
+	return &RoomDriver{gormDb: gormDb}
+}
+
+func (rd *RoomDriver) Create(name string) error {
+	newRoom := domain.Room{Name: name}
+	if err := rd.gormDb.Create(newRoom).Error; err != nil {
+		return &UserRepositoryError{msg: "failed to create room", err: err}
+	}
+	return nil
+}
+
+func (rd *RoomDriver) FindAll() (*[]domain.Room, error) {
+	var rooms []domain.Room
+	res := rd.gormDb.Find(&rooms)
+	if res.Error != nil {
+		return nil, &RoomRepositoryError{msg: "failed to find all rooms", err: res.Error}
+	}
+	return &rooms, nil
+}
+
+func (rd *RoomDriver) Update(room *domain.Room) error {
+	if err := rd.gormDb.Save(room).Error; err != nil {
+		return &UserRepositoryError{msg: fmt.Sprintf("failed to update room: id = %v", room.ID), err: err}
+	}
+	return nil
+}
+
+func (rd *RoomDriver) Delete(room *domain.Room) error {
+	if err := rd.gormDb.Delete(room).Error; err != nil {
+		return &RoomRepositoryError{msg: fmt.Sprintf("failed to delete room: id = %v", room.ID), err: err}
+	}
+	return nil
+}
+
+type RoomRepositoryError struct {
+	msg string
+	err error
+}
+
+func (e *RoomRepositoryError) Error() string {
+	return fmt.Sprintf("error in crud room db %s (%s)", e.msg, e.err)
+}
+
+func (e *RoomRepositoryError) Unwrap() error {
+	return e.err
+}
